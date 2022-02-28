3e02f6c521bea3bc4bd30216ace0fbeda5729218
ANOUSSI <anoussikamla@gmail.com>
2022-02-28 22:40:56 +0100

    Revert "modification du package "com.udemy.demo""
    This reverts commit f35c723332d0436619bba79ee329120ba06fcf8a.

--- src/main/java/com/udemy/demo/SwaggerConfiguration.java
+++ src/main/java/com/udemy/demo/SwaggerConfiguration.java
@@ -15,6 +15,6 @@ public class SwaggerConfiguration  extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-/* comment*/
+
     }
 }
