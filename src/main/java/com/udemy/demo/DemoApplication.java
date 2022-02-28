0b51d1657aad76433f4fccf90a70641e302fcacf
ANOUSSI <64268609+Anoussi-code@users.noreply.github.com>
2022-02-28 13:02:18 +0100

    modification du package "com.udemy.demo" pour acceder a swagger

--- src/main/java/com/udemy/demo/DemoApplication.java
+++ src/main/java/com/udemy/demo/DemoApplication.java
@@ -1,7 +1,16 @@
 package com.udemy.demo;
 
+import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +18,24 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+
+				.build().apiInfo(apiInfo());
+	}
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("REST API")
+				.description("Servicesx")
+				.build();
+
+	}
+
+
 
 }
